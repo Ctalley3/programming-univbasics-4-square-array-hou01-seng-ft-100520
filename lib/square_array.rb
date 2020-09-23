4ba6e76b6ee834fd4ece9e65913f3969a4b2cfff
Crystal T <ctalley.cat@gmail.com>
2020-09-23 17:59:01 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,13 +1,12 @@
-array = [1,2,3,4]
+ array = [1,2,3,4]
 def square_array(array)
   new_array = []
   count = 0
- new_array.push(array[count]**2)
   while count < array.length do
-    puts square_array
+ squared = new_array.push(array[count]**2)
     count += 1
   end
-  return new array
+return new_array
 end
 
 
