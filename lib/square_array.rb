d5545a93d0c4e28c0b72807210bdd229a0d7a637
Crystal T <ctalley.cat@gmail.com>
2020-09-23 17:49:01 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,4 +1,10 @@
 def square_array(array)
- arrays = [81, 100, 256, 625]
+ array = [1,2,3,4]
+def square_array(array)
+  new_array = []
+  count = 0
+  while count < array.length do
+    count += 1
+  end
+Return new_array
 end
-
